f4bdf4182a4dcaa35239d9ef534e0ebeedaa28da
jdolz <j.dolz6549@gmail.com>
2021-04-24 17:26:54 +0200

    equipo.js (model)

--- Models/equipo.js
+++ Models/equipo.js
@@ -2,3 +2,25 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+var User = require('./usuario');
+
+const TeamSchema = Schema({
+    name: String,
+    admin: User,
+    users: {
+        1: { type: User, default: null },
+        2: { type: User, default: null },
+        3: { type: User, default: null },
+        4: { type: User, default: null },
+        5: { type: User, default: null }
+    },
+    image: String,
+    statistics: {
+        victories: { type: Number, default: 0 },
+        defeats: { type: Number, default: 0 },
+        rankingpoints: { type: Number, default: 0 }
+    }
+});
+
+module.exports = mongoose.model('Team', TeamSchema);
