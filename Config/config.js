d360f8289197214ad23c57645192d5767565a365
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-24 20:47:27 +0200

    Añadido enlace mongodb cloud *Comentado en config*

--- Config/config.js
+++ Config/config.js
@@ -2,7 +2,7 @@
 
 const config = {
     port: process.env.PORT || 3700,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/LoA'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/LoA'/*'mongodb+srv://Alexandru:LOGESTA2021@loa.gqmzy.mongodb.net/LoA?retryWrites=true&w=majority'*/
 };
 
 module.exports = config;
