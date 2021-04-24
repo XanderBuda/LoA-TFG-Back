ef6c1ccecfdd00f9f7ed50ed8aa9d9b9efda7c1b
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-24 14:18:10 +0200

    Configurado el index.js para conectar con db y app

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const app = require('./app');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.db)
-    .then(() => {
-        console.log("Conexion a la base de datos establecida");
+mongoose.connect(config.db, (err, res) => {
+    if (err) {
+        return console.log(`Error al conectar a la base de datos: ${err}`);
+    }
+    console.log("Conexion a la base de datos establecida");
+
+    app.listen(config.port, () =>{
+        console.log(`LoA corriendo en http://localhost:${config.port}`);
+    });
+
     })
-    .catch(err => console.log(err));
+   
