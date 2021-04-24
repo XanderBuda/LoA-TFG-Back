ac3cb82812b63401c1f735a2fb5d9d6acd3e0643
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-24 17:43:55 +0200

    Creado el modelo torneo y añadiendo sus atributos

--- Models/torneo.js
+++ Models/torneo.js
@@ -2,3 +2,15 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const User = require('./usuario');
+const Team = require('./equipo');
+
+const TorneoSchema = Schema({
+    admin: User,
+    name: String,
+    logo: String,
+    size: { type: Number, enum: [4,8,16]},
+    teams: { type: Team, maxItems: size}
+});
+
+module.exports = mongoose.model('Tournament',TorneoSchema);
