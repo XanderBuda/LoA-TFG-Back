15faaad5398ab76b3eba63aebf8eb31bb3b8bb30
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-24 17:21:22 +0200

    Creado el esquema de usuario con sus atributos

--- Models/usuario.js
+++ Models/usuario.js
@@ -4,7 +4,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-
+    username: String,
+    picture: String,
+    email: String,
+    password: String,
+    teamRoutes: String,
+    statistics: {
+        victories: { type: Number, default: 0 },
+        defeats:  { type: Number, default: 0 },
+        rankigpoints:  { type: Number, default: 0 }
+    },
+    elo: String,
+    roles: {
+        first: {type: String,enum: ['Toplane','Jungle','Midlane','Adc','Support'] },
+        second: {type: String,enum: ['Toplane','Jungle','Midlane','Adc','Support'] },
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
