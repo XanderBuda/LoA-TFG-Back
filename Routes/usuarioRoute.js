16aaa518e92f7739c0ffa749899cf517958321e5
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-24 20:09:59 +0200

    Añadido el update de las rutas del user

--- Routes/usuarioRoute.js
+++ Routes/usuarioRoute.js
@@ -7,6 +7,7 @@ const userController = require('../Controllers/usuarioController');
 api.get('/users',userController.getUsers);
 api.get('/user/:userId?',userController.getUserById);
 api.post('/user',userController.postUser);
+api.put('/user/:userId',userController.updateUser);
 api.delete('/user/:userId',userController.deleteUser);
 
 module.exports = api;
