cc7e7f0e76e3f0453c6cc100b00f9ab57983fefd
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-25 17:57:55 +0200

    Cambio de orden en las rutas + api.rest

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -3,8 +3,8 @@ const router = Router();
 
 const userController = require('../Controllers/user.controller');
 
-router.get('/:id?', userController.getUserById);
 router.get('/all', userController.getUsers);
+router.get('/:id?', userController.getUserById);
 router.post('/new', userController.newUser);
 router.put('/update/:id', userController.updateUser);
 router.delete('/delete/:id', userController.deleteUser);
