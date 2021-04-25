6255c9839eecded42abf70f55f86d2d24fc38722
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-04-25 16:24:34 +0200

    añadido cors a las dependecias y a la app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require("morgan");
+const cors = require("cors");
 var app = express();
 
 //RUTAS
@@ -15,13 +16,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 //CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+app.use(cors());
 
 //INTERCALADO DE RUTAS
 app.use('', userRoutes);
