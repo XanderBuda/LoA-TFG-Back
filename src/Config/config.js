31c38b9ffd9dc23a9b94fb0c904f5730fafce2fb
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-05-23 18:13:50 +0200

    modificado ruta de la base de datos

--- src/Config/config.js
+++ src/Config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
     port: process.env.PORT || 3700,
-    db: process.env.MONGODB || /*'mongodb://localhost:27017/LoA'*/'mongodb+srv://Alexandru:LOGESTA2021@loa.gqmzy.mongodb.net/LoA?retryWrites=true&w=majority'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/LoA'
 };
 
 module.exports = config;
