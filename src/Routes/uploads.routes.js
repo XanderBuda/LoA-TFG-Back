500b93a40efa5436f88e910358c3aa2caf444957
Alexandru Buda <alexandruaurel.buda@logesta.com>
2021-05-28 16:01:15 +0200

    Comienzo del swagger de rutas

--- src/Routes/uploads.routes.js
+++ src/Routes/uploads.routes.js
@@ -9,6 +9,38 @@ const router = Router();
 
 router.use(expresFileUpload());
 
+/**
+  * @swagger
+  * tags:
+  *   name: Upload
+  *   description: Peticiones sobre subidas de archivos
+  */
+
+/**
+ * @swagger
+ * /upload/{type}:
+ *   put:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Subir una imagen segun el tipo
+ *     tags: [Upload]
+ *     path:
+ *       name: string   
+ *     responses:
+ *       200:
+ *         description: La lista de usuarios
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/User'
+ *       404:
+ *         description: No hay usuarios
+ *       500:
+ *         description: ERROR al realizar la peticion  
+ */
+
 router.put('/:type',validarJWT,uploadController.fileUpload);
 router.get('/:type',validarJWT,uploadController.getFile);
 
